f056b5db696f8d2cf9359c17c0902ebe28e1ff1b
Mohamed Samir Mouawad <114841411+mohamed-samir-dev@users.noreply.github.com>
2026-02-19 15:26:45 +0200

    Remove Console log from developmentversion to production

--- app/pages/contact/components/ContactForm.tsx
+++ app/pages/contact/components/ContactForm.tsx
@@ -14,7 +14,6 @@ export default function ContactForm() {
 
   const handleSubmit = (e: React.FormEvent) => {
     e.preventDefault();
-    console.log('Form submitted:', formData);
   };
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement | HTMLSelectElement>) => {
