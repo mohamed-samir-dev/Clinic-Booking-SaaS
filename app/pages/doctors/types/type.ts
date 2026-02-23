8d7e1574015cf3c6f3c4293b47c75e4592e012ef
Mohamed Samir Mouawad <114841411+mohamed-samir-dev@users.noreply.github.com>
2026-02-23 13:54:32 +0200

    feat(i18n): add language switcher (ar/en)

--- app/pages/doctors/types/type.ts
+++ app/pages/doctors/types/type.ts
@@ -1,21 +1,20 @@
+export interface Specialty {
+  en: string;
+  ar: string;
+}
+
 export interface Filters {
-    specialty: string;
-    gender: string;
-    isAvailableToday: boolean;
-    minExperience: number;
-  }
-  
-  export interface DoctorFiltersProps {
-    filters: Filters;
-    setFilters: (filters: Filters) => void;
-    filterOptions: { specialties: string[]; genders: string[] };
-    showFilters: boolean;
-    setShowFilters: (show: boolean) => void;
-    resetFilters: () => void;
-  }
-  export interface Filters {
-    specialty: string;
-    gender: string;
-    isAvailableToday: boolean;
-    minExperience: number;
-  }
+  specialty: string;
+  gender: string;
+  isAvailableToday: boolean;
+  minExperience: number;
+}
+
+export interface DoctorFiltersProps {
+  filters: Filters;
+  setFilters: (filters: Filters) => void;
+  filterOptions: { specialties: Specialty[]; genders: string[] };
+  showFilters: boolean;
+  setShowFilters: (show: boolean) => void;
+  resetFilters: () => void;
+}
